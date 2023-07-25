0d2c7e6131e49c6d55a3541c52f1e7667ee9eef8
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-07-26 00:37:42 +0530

    .

--- Arrays/minmax.cpp
+++ Arrays/minmax.cpp
@@ -5,28 +5,33 @@ using namespace std;
 int getMax(int arr[], int n)
 {
 
-    int max = INT_MIN;
+    int maxi = INT_MIN;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
+        // using the max and min predefined fxn
+        maxi = max(maxi, arr[i]);
+        // if (arr[i] > max)
+        // {
+        //     max = arr[i];
+        // }
     }
-    return max;
+    return maxi;
 }
 int getMin(int arr[], int n)
 {
 
-    int min = INT_MAX;
+    int mini = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] < min)
-        {
-            min = arr[i];
-        }
+
+        // using the max and min predefined fxn
+        mini = min(mini, arr[i]);
+        // if (arr[i] < min)
+        // {
+        //     min = arr[i];
+        // }
     }
-    return min;
+    return mini;
 }
 
 int main()
