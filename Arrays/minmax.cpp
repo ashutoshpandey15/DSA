f665b19cd2a4db8bea22d18399a832ebcb600413
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-07-26 00:31:25 +0530

    .

---
+++ Arrays/minmax.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int getMax(int arr[], int n)
+{
+
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int getMin(int arr[], int n)
+{
+
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of your array :" << endl;
+    cin >> size;
+    int num[100];
+    // Taking input
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+    }
+
+    cout << "Max Value is : " << getMax(num, size) << endl;
+    cout << "Min Value is : " << getMin(num, size) << endl;
+
+    return 0;
+}
