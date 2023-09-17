b07373933e5e95c14b4fc9c10cb70ded5b2bd9c9
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-09-17 11:25:03 +0530

    selection sort

--- Arrays/uniqueelement.cpp
+++ Arrays/uniqueelement.cpp
@@ -1,14 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
 int findUnique(int *arr, int size)
 {
-    int ans = 0;
-    for (int i = 0; i < size; i++)
-    {
-        ans = ans ^ arr[i];
+   	int ans = 0;
+    
+    for(int i = 0; i<size; i++) {
+        ans = ans^arr[i];
     }
-
     return ans;
 }
