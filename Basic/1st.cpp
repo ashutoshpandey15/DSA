fff4eeded7825ab5015c6ad3df291475af09748d
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-12-21 23:01:06 +0530

    .

--- Basic/1st.cpp
+++ Basic/1st.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main(){
  
-    int n;
-    cin>>n;
-    for (int i = 0; i < n; i++)
-    {
-        cout<<i<<endl;
-    }
-    
-
+   int n;
+   cin>>n;
+   int i =1;
+   int sum = 0;
+   while(i<=n){
+    sum = sum + i;
+    i = i+1;
+    cout<<sum<<" ";
+   }
 }
